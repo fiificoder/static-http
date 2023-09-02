9f6a8155a1451066cec218d6cc99154e1f90b48c
fiificoder <elvisboadi962@gmail.com>
2023-09-02 10:45:31 +0000

    edited slightly

--- main.go
+++ main.go
@@ -19,12 +19,12 @@ func main() {
 	//http://localhost:8000/static/<filename>
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
 
-	srv := &http.Server{
+	server := http.Server{
 		Handler:      r,
 		Addr:         "127.0.0.1:8000",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 
-	log.Fatal(srv.ListenAndServe())
+	log.Fatal(server.ListenAndServe())
 }
