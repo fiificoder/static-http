a0b3cf3c647a8fade090a5491eb8738049b42dd6
fiificoder <elvisboadi962@gmail.com>
2023-09-03 00:06:49 +0000

    using gin web framework

--- main.go
+++ main.go
@@ -1,26 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./Static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/hello", helloHandler)
+	router := gin.Default()
 
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
-}
+	router.StaticFile("/staticfile", "./static/index.html")
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, "method is not supported", http.StatusNotFound)
-		return
-	}
-	fmt.Fprintf(w, "Hello World!!")
+	router.Run(":8080")
+
 }
